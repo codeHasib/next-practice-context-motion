a2694d872bed03541b1849c005273d987c9fc134
codeHasib <mohammadhasib9827@gmail.com>
2026-04-15 21:50:53 +0600

    practice of drag motion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,58 @@
 "use client";
 
-import Image from "next/image";
 import { motion, spring } from "motion/react";
+import { useState } from "react";
 
 export default function Home() {
+  const [score, setScore] = useState(0);
+  const [hit, setHit] = useState(false);
+
+  const target = { x: 150, y: 50, size: 150 };
+
+  const checkInside = (dragX, dragY) => {
+    const isInside =
+      dragX > target.x &&
+      dragX < target.x + target.size &&
+      dragY > target.y &&
+      dragY < target.y + target.size;
+
+    if (isInside && !hit) {
+      setScore((prev) => prev + 1);
+      setHit(true);
+    }
+
+    if (!isInside) {
+      setHit(false);
+    }
+  };
   return (
     <>
+      <div className="min-h-[60vh] bg-green-300 w-full py-4 px-5 font-extrabold relative">
+        <h3 className="text-xl font-bold text-white text-center">
+          Score: {score}
+        </h3>
+        <div
+          className="border-8 border-red-500 absolute"
+          style={{
+            width: target.size,
+            height: target.size,
+            left: target.x,
+            top: target.y,
+            position: "absolute",
+          }}
+        />
+        <motion.div
+          className="h-15 w-15 rounded-full bg-red-300 flex justify-center items-center text-[14px] absolute"
+          initial={{ y: 460, x: 460 }}
+          drag
+          dragConstraints={{ top: 0, bottom: 450, right: 800, left: 0 }}
+          onDrag={(e, info) => {
+            checkInside(info.offset.x, info.offset.y);
+          }}
+        >
+          THROW
+        </motion.div>
+      </div>
       <motion.div className="min-h-screen flex justify-center items-center flex-col gap-[650px] py-50">
         <motion.div
           className="h-90 w-90 bg-white border-8 border-red-400 rounded-4xl"
